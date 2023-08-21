0d42946766d4f1d353d7d858fd6d13054c206a1a
Danny <dannygetch@gmail.com>
2023-08-21 23:12:58 +0300

    task 8

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,13 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Daniel Mekonnen <https://github.com/DannyGetch>
+ */
+class AppController {
+    static getHomepage(request, response) {
+      response.status(200).send('Hello Holberton School!');
+    }
+  }
+  
+  export default AppController;
+  module.exports = AppController;
+  
