e1c905e441b60855a17d5431edc19f7832b34dfe
Danny <dannygetch@gmail.com>
2023-08-21 10:31:01 +0300

    Task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,1 +1,10 @@
+/**
+ * Displays a message that prints in STDOUT.
+ * @param {String} message The message to display.
+ * @author Daniel Mekonnen <https://github.com/DannyGetch>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
 
+module.exports = displayMessage;
